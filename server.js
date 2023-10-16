b062cebb705439dbe69cd114164e1e35ee38c2c0
hermilio <hermilio@illi.com.br>
2023-10-16 11:12:44 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Cliente conectado: ${socket.id}`);
+
+  socket.on("mensagem", (mensagem) => {
+    console.log(`Cliente ${socket.id} enviou uma mensagem: ${mensagem}`);
+    // Enviar a mensagem de volta para o cliente que a enviou
+    socket.emit("mensagem", `Você disse: ${mensagem}`);
+    socket.broadcast.emit("mensagem", `Cliente disse: ${mensagem}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Cliente desconectado: ${socket.id}`);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Servidor Socket.IO rodando na porta 3000");
+});
